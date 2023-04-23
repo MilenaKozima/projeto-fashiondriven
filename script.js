443a8e77fa68684103166db1c3157f9b4d5d9ea4
MilenaKozima <mkozima06@gmail.com>
2023-04-23 14:12:41 -0300

    Versão com o botão funcionando

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+let modelo = '';
+let gola = '';
+let tecido = '';
+let referencia = '';
+
+// seleciona o campo de entrada de texto
+const inputField = document.querySelector('input');
+
+// adiciona um evento de escuta para quando o usuário digitar ou apagar algo
+inputField.addEventListener('input', () => {
+  VerificarSelacao(); // chama a função verificar
+})
+
+function VerificarSelacao(){
+    if (modelo !== ''){
+        if (gola !== ''){
+            if(tecido !== ''){
+                if(document.querySelector('input').value !== ''){
+                const pedidos = document.querySelector('.pedido');
+                pedidos.classList.add('liberabotao');
+                pedidos.removeAttribute('disabled');
+            } else {
+                const pedidos = document.querySelector('.pedido');
+                pedidos.classList.remove('liberabotao');
+                pedidos.addAttribute('disabled');
+            }
+            }
+        }
+    }
+}
+
+function selecionarmodelo(seletor){
+    modelo = seletor.innerHTML
+
+    const selecionadoantes = document.querySelector('.selecionadamodelo');
+
+    if(selecionadoantes !== null){
+        selecionadoantes.classList.remove('selecionadamodelo');
+    }
+
+    seletor.classList.add('selecionadamodelo');
+}
+
+function selecionargola(seletor){
+    gola = seletor.innerHTML
+
+    const selecionadoantes = document.querySelector('.selecionagola');
+
+    if(selecionadoantes !== null){
+        selecionadoantes.classList.remove('selecionagola');
+    }
+
+    seletor.classList.add('selecionagola');
+}
+
+function selecionartecido(seletor){
+    tecido = seletor.innerHTML
+
+    const selecionadoantes = document.querySelector('.selecionadatecido');
+
+    if(selecionadoantes !== null){
+        selecionadoantes.classList.remove('selecionadatecido');
+    }
+
+    seletor.classList.add('selecionadatecido');
+}
+
 function getRequest() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
     promessa.then(processarResposta);
